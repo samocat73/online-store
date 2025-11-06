d3cffd7b88d0fb946024001da706783e5f0745a3
Anton Egorov <mr.egorov.2001@mail.ru>
2025-11-06 19:41:10 +0400

    Completed development

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from catalog.apps import CatalogConfig
+from catalog.views import home_view, contacts_view
+from django.urls import path
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('', home_view, name='home_view'),
+    path('contacts/', contacts_view, name='contacts_view')
+    ]
