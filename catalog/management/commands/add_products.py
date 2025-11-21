cd74606bb2ba5aaed289c1c8fdd4047d72d36406
Anton Egorov <mr.egorov.2001@mail.ru>
2025-11-21 21:22:16 +0400

    feat: I've set up templating

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -11,40 +11,43 @@ class Command(BaseCommand):
             name="Стиральные машины", description="Стиральные машины по выгодным ценам."
         )
         irons, _ = Category.objects.get_or_create(
-            name='Утюги', description='Утюги для ровного белья'
+            name="Утюги", description="Утюги для ровного белья"
         )
 
         products = [
             {
-                'name': 'LG F2Y1NS6W',
-                'description': 'LG F2Y1NS6W - фронтальная стиральная машина с функцией пара.',
-                'category': washing_machines,
-                'price': 27999
+                "name": "LG F2Y1NS6W",
+                "description": "LG F2Y1NS6W - фронтальная стиральная машина с функцией пара.",
+                "category": washing_machines,
+                "price": 27999,
             },
             {
-                'name': 'LG FH0B8LD6',
-                'description': 'Стиральная машина LG FH0B8LD6 рассчитана на одновременную загрузку 5 кг белья.',
-                'category': washing_machines,
-                'price': 33999
+                "name": "LG FH0B8LD6",
+                "description": "Стиральная машина LG FH0B8LD6 рассчитана на одновременную загрузку 5 кг белья.",
+                "category": washing_machines,
+                "price": 33999,
             },
             {
-                'name': 'Polaris PIR 2420AK',
-                'description': 'Утюг Polaris PIR 2420AK в стильном исполнении способен сделать процесс глажки достаточно удобным и быстрым.',
-                'category': irons,
-                'price': 3199
+                "name": "Polaris PIR 2420AK",
+                "description": "Утюг Polaris PIR 2420AK в стильном исполнении способен сделать процесс глажки достаточно удобным и быстрым.",
+                "category": irons,
+                "price": 3199,
             },
             {
-                'name': 'Braun TexStyle 9 SI9661VI',
-                'description': 'Утюг Braun TexStyle 9 SI9661VI с мощностью 3100 Вт, непрерывной подачей пара 0-60 г/мин и паровым ударом до 250 г/мин устраняет стойкие складки.',
-                'category': irons,
-                'price': 9699
-            }
-
+                "name": "Braun TexStyle 9 SI9661VI",
+                "description": "Утюг Braun TexStyle 9 SI9661VI с мощностью 3100 Вт, непрерывной подачей пара 0-60 г/мин и паровым ударом до 250 г/мин устраняет стойкие складки.",
+                "category": irons,
+                "price": 9699,
+            },
         ]
 
         for product_data in products:
             product, created = Product.objects.get_or_create(**product_data)
             if created:
-                self.stdout.write(self.style.SUCCESS(f'Product added successfully: {product.name}'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Product added successfully: {product.name}")
+                )
             else:
-                self.stdout.write(self.style.SUCCESS(f'Product already exists: {product.name}'))
+                self.stdout.write(
+                    self.style.SUCCESS(f"Product already exists: {product.name}")
+                )
--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-def home_view(request):
-    return render(request, "home.html")
+from catalog.models import Product
 
 
 def contacts_view(request):
     return render(request, "contacts.html")
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, "product_detail.html", context=context)
+
+
+def home_view(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "home_view.html", context=context)
