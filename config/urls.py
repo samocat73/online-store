1ef8db9196eb2bfab62f98a0c3406ecad0e2badc
Anton Egorov <mr.egorov.2001@mail.ru>
2025-12-10 20:04:05 +0400

    feat: Added authentication

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
     path("blogs/", include("blog.urls", namespace="blog")),
+    path("users/", include("users.urls", namespace="users")),
 ]
 
 if settings.DEBUG:
